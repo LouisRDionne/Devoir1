141c962235ed692a7350a001b98f339d159b11c8
Louis Robitaille Dionne <glowpixels@gmail.com>
2023-09-13 20:34:58 -0400

    Étape 4 Complétée

--- src/test/java/org/devoir1/domain/CommunityCenterTest.java
+++ src/test/java/org/devoir1/domain/CommunityCenterTest.java
@@ -3,8 +3,6 @@ package org.devoir1.domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CommunityCenterTest {
     // FIFO
     @Test
